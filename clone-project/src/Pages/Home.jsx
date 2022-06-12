7de3d80ad2836a8450297febcc09619ca57f8b81
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-06-12 20:54:32 +0530

    footer

---
+++ clone-project/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footer } from '../components/Footer'
+
+export const Home = () => {
+  return (
+    <div>
+        {/* <h1>Home</h1> */}
+        <h1 style={{fontSize: "25px"}}>Find your dream job now</h1>
+        <p>5 lakh+ jobs for you to explore</p>
+        <Footer />
+    </div>
+  )
+}
