5871842cd08160a9a1add0974074987a85a63304
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-06-15 18:42:01 +0530

    redux

--- clone-project/src/App.js
+++ clone-project/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Navbar } from './components/Navbar';
-import AllRoutes from './components/AllRoutes';
+import {AllRoutes} from './components/AllRoutes';
 // import "slick-carousel/slick/slick.css"; 
 // import "slick-carousel/slick/slick-theme.css";
 
---
+++ clone-project/src/Redux/JobSearch/store.js
@@ -0,0 +1,20 @@
+import { legacy_createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { reducerJobDes } from '../JobDescriptionRedux/reducerJobDes';
+import { jobReducer } from './jobReducer';
+import {recruiterReducer} from "../RecruiterRedux/reducer"
+
+
+const rootReducer = combineReducers({
+    job: jobReducer,
+    jobId:reducerJobDes,
+    recruiter:recruiterReducer
+})
+
+const customMiddleWare = (state) => (next) => (action) => {
+    if (typeof action === 'function') {
+        return action(store.dispatch, store.getState)
+    }
+    return next(action)
+}
+
+export const store = legacy_createStore(rootReducer, compose(applyMiddleware(customMiddleWare), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
